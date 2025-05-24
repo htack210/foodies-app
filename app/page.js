d45932b08f8c349dced155d57f024b09365842cc
Glenn Haertlein <htack210@live.com>
2025-05-24 11:05:49 -0400

    Add slideshow

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
+"use client";
+
 import Link from "next/link";
 
 import hdrClasses from "./page.module.css";
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 const { header, hero, section, slideshow, cta } = hdrClasses;
 
@@ -8,7 +11,9 @@ export default function Home() {
   return (
     <>
       <header className={header}>
-        <div className={slideshow}></div>
+        <div className={slideshow}>
+          <ImageSlideshow />
+        </div>
         <div>
           <div className={hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>

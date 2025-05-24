5886b6adcf0bd6d69109f5936180ee1a02c624e5
Glenn Haertlein <htack210@live.com>
2025-05-24 11:52:32 -0400

    Remove unused component calls.

--- app/community/page.js
+++ app/community/page.js
@@ -1,12 +1,7 @@
-import HomeLink from "@/components/HomeLink";
-
 export default function page() {
   return (
     <main>
       <h1>Community</h1>
-      <p>
-        <HomeLink />
-      </p>
     </main>
   );
 }
--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,12 +1,8 @@
-import HomeLink from "@/components/HomeLink";
 export default function page({ params }) {
   return (
     <main>
       <h1>Meals</h1>
       <p>Recipe: {params.slug}</p>
-      <p>
-        <HomeLink />
-      </p>
     </main>
   );
 }

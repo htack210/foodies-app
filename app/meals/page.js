e0528ae4cc2155572a736255350df83068a9791b
Glenn Haertlein <htack210@live.com>
2025-05-24 15:21:13 -0400

    Output meals data. Add logic for meals grid and css files for meal-items, etc.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,27 @@
+import Link from "next/link";
+
+import mealClasses from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
+const { header, highlight, cta, loading, main } = mealClasses;
 export default function page() {
   return (
-    <main>
-      <h1>Meals</h1>
-    </main>
+    <>
+      <header className={header}>
+        <h1>
+          Delicious meals, created <span className={highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun.
+          (Plus, I ain&apos;t your mama!)
+        </p>
+        <p className={cta}>
+          <Link href="/meals/share">Share your favorite recipes</Link>
+        </p>
+      </header>
+      <main className={main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }

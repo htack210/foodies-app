7b8ef0c0ce0e5730e4b9f4545dc5417360d2b5bd
Glenn Haertlein <htack210@live.com>
2025-05-25 20:29:28 -0400

    Handle not found states.

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Could not find requested page or resource.</p>
+    </main>
+  );
+}

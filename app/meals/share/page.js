9c58ee7a92e0ea4bf83b576c40e28ac873095ff4
Glenn Haertlein <htack210@live.com>
2025-05-26 16:40:32 -0400

    Handle form submissions using server actions.

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,18 +1,35 @@
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 
+const { header, highlight, main, form, row, actions } = classes;
+
 export default function ShareMealPage() {
+  async function shareMeal(formData) {
+    "use server";
+
+    const meal = {
+      title: formData.get("title"),
+      summary: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"),
+      creator: formData.get("name"),
+      creator_email: formData.get("email"),
+    };
+
+    console.log(meal);
+  }
+
   return (
     <>
-      <header className={classes.header}>
+      <header className={header}>
         <h1>
-          Share your <span className={classes.highlight}>favorite meal</span>
+          Share your <span className={highlight}>favorite meal</span>
         </h1>
         <p>Or any other meal you feel needs sharing!</p>
       </header>
-      <main className={classes.main}>
-        <form className={classes.form}>
-          <div className={classes.row}>
+      <main className={main}>
+        <form className={form} action={shareMeal}>
+          <div className={row}>
             <p>
               <label htmlFor="name">Your name</label>
               <input type="text" id="name" name="name" required />
@@ -39,8 +56,8 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
-          <p className={classes.actions}>
+          <ImagePicker label="Your image" name="image" />
+          <p className={actions}>
             <button type="submit">Share Meal</button>
           </p>
         </form>
